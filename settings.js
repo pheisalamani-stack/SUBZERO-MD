26408dd7cbff27e4b8348bedde03bbbbb6ed04a4
pheisalamani-stack <pheisalamani@gmail.com>
2025-09-24 19:30:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;DAsS2LaA#9o-aTFyNoyrO0qi2BI6Jloum_sQSn7Rl3UpUKLK0kIA", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
